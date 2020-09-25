088cd59787ae4f7548b8dd76514e326407c08882
Caio Furtado <62728052+caiohbfurtado@users.noreply.github.com>
2020-09-25 18:01:01 -0300

    create modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Modal({ children }) {
+  return <S.Container>{children}</S.Container>;
+}
+
+export default Modal;
---
+++ src/components/Modal/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #0008;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
