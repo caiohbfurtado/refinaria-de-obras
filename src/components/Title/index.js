02070f6cba25fcd6e1b2c83d79f6a1ab651387c2
Caio Furtado <62728052+caiohbfurtado@users.noreply.github.com>
2020-09-24 21:20:30 -0300

    create Title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Title({ children }) {
+  return <S.Title>{children}</S.Title>;
+}
+
+export default Title;
---
+++ src/components/Title/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  line-height: 1;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 28px;
+  text-transform: uppercase;
+`;
