63a1f9f56f6e3a76c87dd38b7a9b9e751407999c
Caio Furtado <62728052+caiohbfurtado@users.noreply.github.com>
2020-09-24 21:20:46 -0300

    create search container

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Title from '../components/Title';
+import Button from '../components/Button';
+
 import * as S from './styles';
 
 function App() {
+  const [type, setType] = useState('');
+  const [title, setTitle] = useState('');
+
   return (
     <S.Container>
-      <h1>Olá!</h1>
+      <S.ContainerSearch>
+        <Title>Pesquisar</Title>
+        <S.Subtitle>O que deseja buscar?</S.Subtitle>
+        <S.Buttons>
+          <Button title="Filmes" onClick={() => setType('movie')} />
+          <Button title="séries" onClick={() => setType('series')} />
+        </S.Buttons>
+        <S.Input
+          placeholder="Informe o título da obra"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Button title="pesquisar" color />
+      </S.ContainerSearch>
+      <S.ResultsContainer>
+        <Title>resultados</Title>
+      </S.ResultsContainer>
     </S.Container>
   );
 }
--- src/App/styles.js
+++ src/App/styles.js
@@ -1,3 +1,46 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  height: 100%;
+  flex: 1;
+`;
+
+export const ContainerSearch = styled.div`
+  height: 100%;
+  width: 350px;
+  padding: 20px 10px;
+`;
+
+export const ResultsContainer = styled.div`
+  display: flex;
+  flex: 1;
+  background: blue;
+  padding: 20px 20px;
+`;
+
+export const Subtitle = styled.p`
+  margin: 25px 0 15px;
+  font-size: 16px;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+`;
+
+export const Input = styled.input.attrs(({ placeholder, value, onChange }) => ({
+  type: 'text',
+  placeholder,
+  value,
+  onChange,
+}))`
+  width: 100%;
+  height: 40px;
+  padding: 10px;
+  background: transparent;
+  outline: none;
+  border: 1px solid #000;
+  margin-bottom: 15px;
+`;
